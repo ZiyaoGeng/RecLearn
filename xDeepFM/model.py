45fd121d62b2eed0e5fb2989669cce0f385a48f2
gzy <593947521@qq.com>
2020-08-24 20:10:39 +0800

    Update 8.24

--- xDeepFM/model.py
+++ xDeepFM/model.py
@@ -8,21 +8,22 @@ model: xDeepFM: Combining Explicit and Implicit Feature Interactions for Recomme
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers, optimizers, losses
+from tensorflow.keras import layers
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import Embedding, Dropout, Flatten, Dense, Input
-
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class DNN(layers.Layer):
     """
     DNN part
     """
-
     def __init__(self, hidden_units, dnn_dropout, dnn_activation):
+        """
+
+        :param hidden_units: list of hidden layer units's numbers
+        :param dnn_dropout: dropout number
+        :param dnn_activation: activation function
+        """
         super(DNN, self).__init__()
         self.dnn_network = [Dense(units=unit, activation=dnn_activation) for unit in hidden_units]
         self.dropout = Dropout(dnn_dropout)
@@ -39,7 +40,6 @@ class Linear(layers.Layer):
     """
     Linear Part
     """
-
     def __init__(self):
         super(Linear, self).__init__()
         self.dense = Dense(1, activation=None)
@@ -53,10 +53,15 @@ class CIN(layers.Layer):
     """
     CIN part
     """
+    def __init__(self, cin_size, l2_reg=1e-4):
+        """
 
-    def __init__(self, cin_size, l2_reg):
+        :param cin_size: [H_1, H_2 ,..., H_k], a list of the number of layers
+        :param l2_reg:
+        """
         super(CIN, self).__init__()
         self.cin_size = cin_size
+        self.l2_reg = l2_reg
 
     def build(self, input_shape):
         self.embedding_nums = input_shape[1]
@@ -68,7 +73,7 @@ class CIN(layers.Layer):
                 name='CIN_W_' + str(i),
                 shape=(1, self.field_nums[0] * self.field_nums[i], self.field_nums[i + 1]),
                 initializer='random_uniform',
-                regularizer=l2(0.00001),
+                regularizer=l2(self.l2_reg),
                 trainable=True)
             for i in range(len(self.field_nums) - 1)
         }
@@ -104,11 +109,22 @@ class CIN(layers.Layer):
 class xDeepFM(keras.Model):
     def __init__(self, feature_columns, hidden_units=(200, 200), cin_size=(128, 128,), dnn_dropout=0, dnn_activation='relu',
                  embed_reg=1e-5, cin_reg=1e-5):
+        """
+        xDeepFM architecture
+        :param feature_columns: a list containing dense and sparse column feature information
+        :param hidden_units: a list of dnn hidden units
+        :param cin_size: a list of the number of CIN layers
+        :param dnn_dropout: dropout of dnn
+        :param dnn_activation: activation function of dnn
+        :param embed_reg: the regularizer of embedding
+        :param cin_reg: the regularizer of cin
+        """
         super(xDeepFM, self).__init__()
         self.dense_feature_columns, self.sparse_feature_columns = feature_columns
         self.embed_dim = self.sparse_feature_columns[0]['embed_dim']
         self.embed_layers = {
-            'embed_' + str(i): Embedding(input_dim=feat['feat_num'], input_length=1,
+            'embed_' + str(i): Embedding(input_dim=feat['feat_num'],
+                                         input_length=1,
                                          output_dim=feat['embed_dim'],
                                          embeddings_initializer='random_uniform',
                                          embeddings_regularizer=l2(embed_reg))
@@ -121,12 +137,17 @@ class xDeepFM(keras.Model):
 
     def call(self, inputs, **kwargs):
         dense_inputs, sparse_inputs = inputs
+        # linear
         linear_out = self.linear(sparse_inputs)
+
         embed = [self.embed_layers['embed_{}'.format(i)](sparse_inputs[:, i]) for i in range(sparse_inputs.shape[1])]
+        # cin
         embed_matrix = tf.transpose(tf.convert_to_tensor(embed), [1, 0, 2])
         cin_out = self.cin(embed_matrix)
+        # dnn
         embed_vector = tf.reshape(embed_matrix, shape=(-1, embed_matrix.shape[1] * embed_matrix.shape[2]))
         dnn_out = self.dnn(embed_vector)
+
         total_input = tf.concat([linear_out, cin_out, dnn_out, dense_inputs], axis=-1)
         output = tf.nn.sigmoid(self.dense(total_input))
         return output
@@ -135,14 +156,3 @@ class xDeepFM(keras.Model):
         dense_inputs = Input(shape=(len(self.dense_feature_columns),), dtype=tf.float32)
         sparse_inputs = Input(shape=(len(self.sparse_feature_columns),), dtype=tf.int32)
         keras.Model(inputs=[dense_inputs, sparse_inputs], outputs=self.call([dense_inputs, sparse_inputs])).summary()
-
-
-def main():
-    dense = [{'name': 'c1'}, {'name': 'c2'}, {'name': 'c3'}, {'name': 'c4'}]
-    sparse = [{'feat_num': 100, 'embed_dim': 16}, {'feat_num': 200, 'embed_dim': 16}]
-    columns = [dense, sparse]
-    model = xDeepFM(columns, [200, 200, 200])
-    model.summary()
-
-
-# main()
--- xDeepFM/train.py
+++ xDeepFM/train.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import AUC
-from utils import create_dataset
+from utils import create_criteo_dataset
 from model import xDeepFM
 
 import os
@@ -18,20 +18,29 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-def main(sample_num, embed_dim, learning_rate, epochs, batch_size, hidden_units=(200, 200), cin_size=(128, 128)):
-    """
+if __name__ == '__main__':
+    # ========================= Hyper Parameters =======================
+    file = '../dataset/Criteo/train.txt'
+    read_part = True
+    sample_num = 100000
+    test_size = 0.2
 
-    :param sample_num: the num of training sample
-    :param embed_dim: the dimension of all embedding layer
-    :param learning_rate:
-    :param epochs:
-    :param batch_size:
-    :param hidden_units:
-    :param cin_size:
-    :return:
-    """
-    feature_columns, train_X, test_X, train_y, test_y = create_dataset(sample_num, embed_dim)
+    embed_dim = 8
+    dnn_dropout = 0.5
+    hidden_units = [256, 128, 64]
+    cin_size = (128, 128)
 
+    learning_rate = 0.001
+    batch_size = 512
+    epochs = 5
+    # ========================== Create dataset =======================
+    feature_columns, train, test = create_criteo_dataset(file=file,
+                                                         embed_dim=embed_dim,
+                                                         read_part=read_part,
+                                                         sample_num=sample_num,
+                                                         test_size=test_size)
+    train_X, train_y = train
+    test_X, test_y = train
     # ============================Build Model==========================
     model = xDeepFM(feature_columns, hidden_units, cin_size)
     model.summary()
@@ -53,14 +62,3 @@ def main(sample_num, embed_dim, learning_rate, epochs, batch_size, hidden_units=
     )
     # ===========================Test==============================
     print('test AUC: %f' % model.evaluate(test_X, test_y)[1])
-
-
-if __name__ == '__main__':
-    sample_num = 100000
-    embed_dim = 16
-    learning_rate = 0.001
-    epochs = 5
-    batch_size = 512
-    hidden_units = [200, 200]
-    cin_size = (128, 128)
-    main(sample_num, embed_dim, learning_rate, epochs, batch_size, hidden_units, cin_size)
