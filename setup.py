42f0419d2cdc80a2397fdb2326cf30e19e290b73
ZiyaoGeng <593947521@qq.com>
2021-11-17 21:22:54 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="reclearn",
-  version="1.0.0",
+  version="1.0.1",
   author="Ziyao Geng",
   author_email="zggzy1996@163.com",
   description="A simple package about learning recommendation",
