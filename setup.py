bd14d8c1a06d6aa28bbf15f1312d00a04b4227ae
ZiyaoGeng <593947521@qq.com>
2022-04-23 20:37:11 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="reclearn",
-  version="1.0.16",
+  version="1.1.0",
   author="Ziyao Geng",
   author_email="zggzy1996@163.com",
   description="A simple package about learning recommendation",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/ZiyaoGeng/Recommender-System-with-TF2.0",
+  url="https://github.com/ZiyaoGeng/RecLearn",
   packages=setuptools.find_packages(),
-  python_requires=">=3.6",
+  python_requires=">=3.8",
   classifiers=[
   "Programming Language :: Python :: 3",
   "License :: OSI Approved :: MIT License",
